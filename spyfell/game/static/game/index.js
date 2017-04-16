3ce571219c1596aac003f14eadd34e1c8dbdad41
Ben <doc.ben.johnson@gmail.com>
2017-04-16 16:38:10 -0400

    session UI

--- spyfell/game/static/game/index.js
+++ spyfell/game/static/game/index.js
@@ -1,11 +1,3 @@
 $(document).ready(() => {
-    console.log("hi there")
-    sessions = [
-        'one',
-        'two',
-        'three'
-    ]
-    sessions.forEach((element) =>{
-        $('#sessionList').append('<li>' + element + '</li>');
-    });
+
 })
--- spyfell/game/views.py
+++ spyfell/game/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Session
+
 
 def index(request):
-    return render(request, 'game/index.html', None)
+    sessions = Session.objects.order_by('name')
+    context = {
+        'sessions': sessions
+    }
+    return render(request, 'game/index.html', context)
 
 
 class SessionList(APIView):
