a13612d7af792e5228e5bce8b301facfa8a81b73
Ben <doc.ben.johnson@gmail.com>
2017-04-16 15:42:21 -0400

    rest endpoint working

--- spyfell/spyfell/urls.py
+++ spyfell/spyfell/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from spyfell.views import index
+from spyfell import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index)
+    # url(r'^', include(router.urls)),
+    url(r'^$', views.index),
+    url(r'^sessions/', views.SessionList.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
