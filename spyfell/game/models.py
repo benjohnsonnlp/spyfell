57ce86c5c77828ec7d41502f14ad1909dc670b66
Ben <doc.ben.johnson@gmail.com>
2017-04-16 16:27:12 -0400

    admin and model updates

--- spyfell/game/models.py
+++ spyfell/game/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
-class Sessions(models.Model):
+class Session(models.Model):
     name = models.CharField(max_length=300, default='Another Spyfell Session')
     created = models.DateTimeField('created')
+
+    def __str__(self):
+        return self.name
