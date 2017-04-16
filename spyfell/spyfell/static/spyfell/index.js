8b0295204c244561bf3d0825023c02896b938191
Ben <doc.ben.johnson@gmail.com>
2017-04-16 15:03:34 -0400

    styling + django rest requirement

--- spyfell/spyfell/static/spyfell/index.js
+++ spyfell/spyfell/static/spyfell/index.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
     console.log("hi there")
+    sessions = [
+        'one',
+        'two',
+        'three'
+    ]
+    sessions.forEach((element) =>{
+        $('#sessionList').append('<li>' + element + '</li>');
+    });
 })
